bf4b21c3e7c7d292509becd50b3bb81cd9ed1611
HimbeersaftLP <HimbeersaftLP@users.noreply.github.com>
2016-12-23 17:19:19 +0100

    Allow HEX color values

--- sofeavatar.php
+++ sofeavatar.php
@@ -2,13 +2,29 @@
 
 header("Content-type: image/png");
 
-$r = $_GET['r'];
-$g = $_GET['g'];
-$b = $_GET['b'];
+function hexrgb($hex) {
+    $hex = str_replace("#", "", $hex);
+    if(strlen($hex) == 3) {
+      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+    } else {
+      $r = hexdec(substr($hex,0,2));
+      $g = hexdec(substr($hex,2,2));
+      $b = hexdec(substr($hex,4,2));
+    }
+    return array("r" => $r, "g" => $g, "b" => $b);
+}
+
+if(isset($_GET["hex"])){
+  $rgb = hexrgb($_GET["hex"]);
+}else{
+  $rgb = array("r" => $_GET["r"], "g" => $_GET["g"], "b" => $_GET["b"]);
+}
 
 $img = imagecreatetruecolor(120, 120);
 $bg = imagecolorallocate($img, 237, 237, 237);
-$color = imagecolorallocate($img, $r, $g, $b);
+$color = imagecolorallocate($img, $rgb["r"], $rgb["g"], $rgb["b"]);
 
 imagefill($img, 0, 0, $bg);
 
