53adc42199529aacc1487e51ddbe1d1ce610e018
berkutta <info@benjaminmarty.ch>
2016-12-26 00:48:47 +0100

    Change JQuery adressing to now existing proper id's

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function run() {
-	$("input[name=hex], input[name=bghex], input[name=rot]").change(function() {
-		$("img[name=avatar]").attr("src", "sofeavatar.php?hex=" + $("input[name=hex]").val() + "&bghex=" + $("input[name=bghex]").val() + "&rot=" + $("input[name=rot]").val());
+	$("#color, #bgcolor, #rotation").change(function() {
+		$("img[name=avatar]").attr("src", "sofeavatar.php?hex=" + $("#color").val() + "&bghex=" + $("#bgcolor").val() + "&rot=" + $("#rotation").val());
 	});
 }
