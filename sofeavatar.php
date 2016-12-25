af1d4f6615bb4038d64f42954db47e8a7dd33233
Thunder33345 <Thunder33345@users.noreply.github.com>
2016-12-26 00:53:06 +0800

    allowed direct angle of rotation

--- sofeavatar.php
+++ sofeavatar.php
@@ -47,9 +47,10 @@ imagefilledrectangle($img, 70, 70, 90, 90, $color);
 imagefilledrectangle($img, 10, 90, 110, 110, $color);
 if(isset($_GET['rot'])) $rot = $_GET['rot']; else $rot = 0;
 if(ctype_digit($rot) AND $rot >= 0 AND $rot <= 3){
-  if ($rot == 1) $rot = 90;
-  if ($rot == 2) $rot = 180;
-  if ($rot == 3) $rot = 270;
+  if ($rot == 1 OR $rot = 90) $rot = 90;
+  elseif ($rot == 2 OR $rot = 180) $rot = 180;
+  elseif ($rot == 3 OR $rot = 270) $rot = 270;
+  else $rot = 0;
 } else $rot = 0;
 
 if ($rot == 0)
