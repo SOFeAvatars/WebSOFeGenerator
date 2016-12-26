95570bfcab38989233df0bb6c7ee0eeab3a15d3a
Thunder33345 <Thunder33345@users.noreply.github.com>
2016-12-26 14:09:17 +0800

    Update sofeavatar.php

--- sofeavatar.php
+++ sofeavatar.php
@@ -1,10 +1,5 @@
 <?php
 
-if( empty($_GET["hex"]) || empty($_GET["bghex"]) ) {
-  echo "Error";
-  return 0;
-}
-
 header('Set-Cookie: fileDownload=true; path=/');
 header("Content-type: image/png");
 header('Content-Disposition: attachment; filename="'.$_GET["hex"].'_'.$_GET["bghex"].'_'.$_GET['rot'].'.png"');
